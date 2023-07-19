b1924a9a98132d62da523bce82698e0dc3570833
krzyzinskim <mateusz_krzyzinski@wp.pl>
2023-07-19 15:00:01 +0200

    update example

--- rashomon_detect/example.R
+++ rashomon_detect/example.R
@@ -3,6 +3,7 @@ source("model_selection.R")
 source("pdi_measures.R")
 source("performance_calculation.R")
 source("profile_dissimilarity_calculation.R")
+source("plot.R")
 source("rashomon_detect.R")
 
 library(ranger)
@@ -10,7 +11,7 @@ library(randomForest)
 library(gbm)
 
 X <- matrix(rnorm(2000*10), ncol=10)
-y <- as.numeric(rowSums(X^2) > qchisq(0.5, 10))
+y <- as.numeric(rowSums(X) + X[,1]^2 - X[,2]^2 + rnorm(2000) > 0)
 
 train_indices <- sample(1:length(y), 1600)
 
@@ -25,6 +26,15 @@ model2 = ranger(y ~ ., data = df_train, classification = TRUE, num.trees=100)
 model3 = randomForest(factor(y) ~ ., data = df_train, ntree=100)
 model4 = gbm(y ~ ., data = df_train, distribution = "bernoulli")
 
-explainers <- make_explainer_list(model1, model2, model3, model4, data = df_test[, 1:10], y = df_test[, 11])
-res <- rashomon_detect(explainers, epsilon=0.1, k=3, pdi_method = derivative_euclidean_distance)
+explainers <- make_explainer_list(model1, model2, model3, model4, 
+                                  data = df_test[, 1:10], y = df_test[, 11])
+
+res <- rashomon_detect(explainers, 
+                       k=3, 
+                       pdi_method = derivative_euclidean_distance, 
+                       comparison = "last"
+                      )
 res
+
+plot_summary_matrix(res)
+
